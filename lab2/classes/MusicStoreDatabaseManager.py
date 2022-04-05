e1c03d2098755db033d1a7da0ca6c76aa1f588b6
vladkorosko <54815768+vladkorosko@users.noreply.github.com>
2022-04-05 16:45:45 +0300

    Lab2: added finding data by criteria

--- lab2/classes/MusicStoreDatabaseManager.py
+++ lab2/classes/MusicStoreDatabaseManager.py
@@ -35,7 +35,10 @@ class MusicStoreDataBaseManager:
             print("Error while getting list of albums:", e)
 
     def add_new_author(self, author_id: int, name: str):
-        query = "INSERT INTO AUTHORS (ID_AUTHOR, NAME) VALUES (" + str(author_id) + ", " + name + ")"
+        values = str(author_id) + ", '" + name + "'"
+
+        query = "INSERT INTO AUTHORS (ID_AUTHOR, NAME) VALUES(" + values + ")"
+
         try:
             self.cursor.execute(query)
             self.db.commit()
@@ -47,8 +50,10 @@ class MusicStoreDataBaseManager:
             return False
 
     def add_new_album(self, album_id: int, name: str, number_of_songs: int, author_id: int):
-        query = "INSERT INTO AUTHORS (ID_ALBUM, NAME, NUMBER_OF_SONGS, ID_AUTHOR) VALUES (" + \
-                str(album_id) + ", " + name + ", " + str(number_of_songs) + ", " + str(author_id) + ")"
+        values = str(album_id) + ", " + str(author_id) + ", '" + name + "', " + str(number_of_songs)
+        query = "INSERT INTO ALBUMS (ID_ALBUM, ID_AUTHOR, NAME, NUMBER_OF_SONGS) VALUES (" + values + ")"
+
+        print(query)
         try:
             self.cursor.execute(query)
             self.db.commit()
@@ -60,7 +65,7 @@ class MusicStoreDataBaseManager:
             return False
 
     def change_author(self, author_id: int, name: str):
-        query = "UPDATE AUTHORS SET NAME = " + name + " WHERE ID_AUTHOR = " + str(author_id)
+        query = "UPDATE AUTHORS SET NAME = '" + name + "' WHERE ID_AUTHOR = " + str(author_id)
         try:
             self.cursor.execute(query)
             self.db.commit()
@@ -72,7 +77,7 @@ class MusicStoreDataBaseManager:
             return False
 
     def change_album(self, album_id: int, name: str, number_of_songs: int, author_id: int):
-        query = "UPDATE ALBUMS SET NAME = " + name + ", NUMBER_OF_SONGS = " + str(number_of_songs) + \
+        query = "UPDATE ALBUMS SET NAME = '" + name + "', NUMBER_OF_SONGS = " + str(number_of_songs) + \
                 ", ID_AUTHOR = " + str(author_id) + " WHERE ID_ALBUM = " + str(album_id)
         try:
             self.cursor.execute(query)
@@ -109,3 +114,102 @@ class MusicStoreDataBaseManager:
             print("Error while deleting album:", e)
             self.db.rollback()
             return False
+
+    def find_author_by_condition(self, field_name, field_value, field_output):
+        adding = False
+        if field_name == "id":
+            finding_by = "ID_AUTHOR"
+        elif field_name == "name":
+            finding_by = "NAME"
+            adding = True
+        else:
+            raise Exception("Wrong name of field")
+
+        if field_output == "*":
+            select_field = "*"
+        elif field_output == "id":
+            select_field = "ID_AUTHOR"
+        elif field_output == "name":
+            select_field = "NAME"
+        else:
+            raise Exception("Wrong name of field")
+
+        if adding:
+            query = "SELECT " + select_field + " FROM AUTHORS WHERE " + \
+                    finding_by + " = " + "'" + field_value + "'"
+        else:
+            query = "SELECT " + select_field + " FROM AUTHORS WHERE " + \
+                    finding_by + " = " + str(field_value)
+
+        try:
+            self.cursor.execute(query)
+            results = self.cursor.fetchall()
+            print("The authors are: ")
+            if field_output == "name":
+                for row in results:
+                    print("Author name:", row[0])
+            elif field_output == "id":
+                for row in results:
+                    print("Author id:", row[0])
+            else:
+                for row in results:
+                    print("Author id:", row[0], "| Author name:", row[1])
+        except Exception as e:
+            print("Error while getting list of authors:", e)
+
+    def find_album_by_condition(self, field_name, field_value, field_output):
+        adding = False
+        if field_name == "album_id":
+            finding_by = "ID_ALBUM"
+        elif field_name == "name":
+            finding_by = "NAME"
+            adding = True
+        elif field_name == "number_of_songs":
+            finding_by = "NUMBER_OF_SONGS"
+        elif field_name == "author_id":
+            finding_by = "ID_AUTHOR"
+        else:
+            raise Exception("Wrong name of field")
+
+        if field_output == "album_id":
+            select_field = "ID_ALBUM"
+        elif field_output == "name":
+            select_field = "NAME"
+        elif field_output == "number_of_songs":
+            select_field = "NUMBER_OF_SONGS"
+        elif field_output == "author_id":
+            select_field = "ID_AUTHOR"
+        elif field_output == "*":
+            select_field = "*"
+        else:
+            raise Exception("Wrong name of field")
+
+        if adding:
+            query = "SELECT " + select_field + " FROM ALBUMS WHERE " + \
+                    finding_by + " = " + "'" + field_value + "'"
+        else:
+            query = "SELECT " + select_field + " FROM ALBUMS WHERE " + \
+                    finding_by + " = " + str(field_value)
+
+        try:
+            self.cursor.execute(query)
+            results = self.cursor.fetchall()
+            print("The albums are: ")
+            if field_output == "album_id":
+                for row in results:
+                    print("Album id:", row[0])
+            elif field_output == "name":
+                for row in results:
+                    print("Album name:", row[0])
+            elif field_output == "number_of_songs":
+                for row in results:
+                    print("Number of songs:", row[0])
+            elif field_output == "author_id":
+                for row in results:
+                    print("Author id:", row[0])
+            else:
+                for row in results:
+                    print("Album id:", row[0], "| Album name:", row[1],
+                          "| Number of songs:", row[2], "| Author id:", row[3])
+        except Exception as e:
+            print("Error while getting list of albums:", e)
